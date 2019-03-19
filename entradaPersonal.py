0eae46242478a0bb042c64343dbb5217117f3fcf
Jose Angel Cardeña Rosel <josonerop99@gmail.com>
2019-03-19 16:35:23 -0600

    programas de entrada de datos
    hicimos las entradas de nombres y numeros.

---
+++ entradaPersonal.py
@@ -0,0 +1,12 @@
+# #CAPTURAMOS LA ENTRADA DE TECLADO Y LA GUARDAMOS EN UNA VARIABLE.
+# nombre = input("ingresar Nombre:")
+# apellido = input("ingresar su apellido:")
+# edad = input("ingresar su edad")
+#
+# #imprimimos lo capturdo por teclado
+# print(f"bienvenido {nombre} {apellido}, su edad es: {edad}")
+
+numero1 = int(input("ingrese un numero "))
+numero2 = float(input("ingrese otro numero "))
+resultado = numero1 + numero2
+print(f"El resultado es: {resultado} ")
