2803c83457754525e6c85027ee8024dc90caab15
Jose Angel Cardeña Rosel <josonerop99@gmail.com>
2019-03-19 18:11:42 -0600

    TODOS LOS CICLOS
    se vieron todas las formas de ver los ciclos.

---
+++ ciclos.py
@@ -0,0 +1,22 @@
+#ciclo WHILE
+a = 0
+b = 100
+# c = 0
+
+while(a<b):
+    a+=1
+    # print(c)
+
+#ciclos FOR
+
+# for x in range(0, 3000, 5):
+#         print(x)
+
+
+    #ciclo "FOR EACH"
+lista = ["manzana", "pera", "platano", "sandia","uva", "melon"]
+for elemento in lista:
+        if(elemento=="melon"):
+            print(elemento)
+
+print(len(lista))
